237561a76873a1283cb2d1de258cf2d7ac3b3fe7
Arif12 <arif.uisc@gmail.com>
2014-12-26 00:53:13 +0600

    Update new.php

--- new.php
+++ new.php
@@ -15,3 +15,25 @@ mysqli_query($link, $query);
 header('location:list.php');
  ?>
  <a href="ftfl.html"> Another entry </a>
+
+
+
+<?php
+$accesscode= $_POST['accesscode'];
+$track= $_POST['track'];
+$fullname= $_POST['fullname'];
+$fathersname=$_POST['fathersname'];
+$mothersname=$_POST['mothersname'];
+$religion=$_POST['religion'];
+
+$link= mysqli_connect("localhost",
+						"root",
+						"",
+						"ftfl");
+
+$query = "INSERT INTO `ftfl`.`users` (`id`, `access_code`, `preferred_track`, `full_name`,`fathers_name`,`mothers_name`,`religion`) VALUES ('NULL', '$accesscode', '$track', '$fullname','$fathersname','$mothersname','$religion');";
+
+mysqli_query($link, $query);
+header('location:list.php');
+ ?>
+ <a href="ftfl.html"> Another entry </a>
