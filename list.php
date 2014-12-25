4dfc32589a34b7799c1350d92feb339dcb1d30d5
Arif12 <arif.uisc@gmail.com>
2014-12-26 00:54:22 +0600

    Update list.php

--- list.php
+++ list.php
@@ -37,3 +37,44 @@ $result= mysqli_query($link,$query);
 ?>
 </table>
 <a href="ftfl.html">HOME</a>
+
+
+
+<?php
+$link= mysqli_connect("localhost",
+						"root",
+						"",
+						"ftfl");
+
+$query="SELECT * FROM users";
+
+$result= mysqli_query($link,$query);
+?>
+
+<table border="1" width="70%">
+  <tr>
+    <td> id </td>
+    <td> Access Code</td>
+    <td> Preferred Track </td>
+    <td> Full Name </td>
+    <td> Fathers Name </td>
+    <td> Mothers Name </td>
+    <td>Religion</td>
+    <td>Action</td>
+  </tr>
+
+<?php
+     foreach($result as $row) {
+?>
+<tr>
+	<td><?php echo $row['id']?></td>
+    <td><?php echo $row['access_code']?></td>
+    <td><?php echo $row['preferred_track']?></td>
+    <td><?php echo $row['full_name']?></td>
+    <td><?php echo $row['fathers_name']?></td>
+    <td><?php echo $row['mothers_name']?></td>
+    <td><?php echo $row['religion']?></td>
+            <td> <a href="edit.php">Edit</a> | <a href="delete.php?id=<?php echo $row['id']?>">Delete</a> | <a href="view.php?id=<?php echo $row['id']?>">View</a></td>
+</tr>
+
+<?php
